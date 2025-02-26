87260904c150932bef22f2a0f62ec30edb619978
Mohd Meraj Ansari <169152384+mohdmerajansari@users.noreply.github.com>
2025-02-26 11:01:51 +0530

    Update Login.jsx

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -12,7 +12,7 @@ function Login() {
   const handleLogin = async (e) => {
     e.preventDefault();
    try {
-      const response = await fetch("http://localhost:3000/api/users/login", {
+      const response = await fetch("https://mern-auth-3-api.vercel.app/api/users/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email, password }),
